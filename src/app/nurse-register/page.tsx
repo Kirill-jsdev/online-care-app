2c7eb8f8b84a24929f6287ee1e2e033f0a99d5bc
Kirill Zlobin <jsdevzk@gmail.com>
2025-10-21 17:30:17 +0200

    Created NurseRegisterPage

---
+++ src/app/nurse-register/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function NurseRegisterPage() {
+  return (
+    <div>
+      <h1>Nurse Register Page</h1>
+    </div>
+  );
+}

3295243ee7ec22db3b5b4394d8b5f6e94d562e1f
Kirill Zlobin <jsdevzk@gmail.com>
2025-10-22 00:12:08 +0200

    Continued...

--- src/app/nurse-register/page.tsx
+++ src/app/nurse-register/page.tsx
@@ -3,20 +3,29 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+type NurseRegisterFormData = {
+  first_name: string;
+  last_name: string;
+  experience_since: string;
+};
 
 export default function NurseRegisterPage() {
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    alert("Form submitted");
+  const { register, handleSubmit } = useForm<NurseRegisterFormData>();
+
+  const onSubmit = (data: NurseRegisterFormData) => {
+    console.log(data.experience_since);
   };
+
   return (
     <div className="p-2">
       <h1>Nurse Register Page</h1>
 
-      <form onSubmit={handleSubmit} noValidate className="flex flex-col gap-4 max-w-sm">
-        <Input type="text" name="first_name" placeholder="First Name" required />
-        <Input type="text" name="last_name" placeholder="Last Name" required />
-        <Input type="date" name="experience_since" placeholder="Experience Since" required />
+      <form onSubmit={handleSubmit(onSubmit)} noValidate className="flex flex-col gap-4 max-w-sm">
+        <Input type="text" {...register("first_name")} placeholder="First Name" required />
+        <Input type="text" {...register("last_name")} placeholder="Last Name" required />
+        <Input type="date" {...register("experience_since")} placeholder="Experience Since" required />
         <Button type="submit">Register</Button>
       </form>
     </div>

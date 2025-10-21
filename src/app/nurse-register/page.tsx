baa636a23d5a34f0557ac4f24985e33e2a451c87
Kirill Zlobin <jsdevzk@gmail.com>
2025-10-21 23:27:33 +0200

    Continued...

--- src/app/nurse-register/page.tsx
+++ src/app/nurse-register/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input";
 import React from "react";
 

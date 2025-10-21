67f8c22c7bbb5b6e8eb46793ed6b83e6d07619e4
Kirill Zlobin <jsdevzk@gmail.com>
2025-10-21 22:59:13 +0200

    Continued...

--- src/app/nurse-register/page.tsx
+++ src/app/nurse-register/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import React from "react";
 
 export default function NurseRegisterPage() {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Handle form submission logic here
+  };
   return (
     <div className="p-2">
       <h1>Nurse Register Page</h1>
+
+      <form onSubmit={handleSubmit} noValidate className="flex flex-col gap-4 max-w-sm">
+        <Input type="text" name="first_name" placeholder="First Name" required />
+        <Input type="text" name="last_name" placeholder="Last Name" required />
+        <Button type="submit">Register</Button>
+      </form>
     </div>
   );
 }
